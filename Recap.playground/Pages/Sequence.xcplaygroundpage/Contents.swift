01df77a0bb01910b588f0bd0e0a643504c2bd019
Student <student@MacBook-Pro-856-Anderson.local>
2017-03-28 10:41:32 -0400

    finished the first part of the recap which is the sequence

--- Recap.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
@@ -27,11 +27,27 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 400)
 
-// Replace this comment and add your code below 
+canvas.drawShapesWithBorders = false
+//draw ellipse 
+canvas.fillColor = Color.red
+canvas.drawEllipse(centreX: 120, centreY: 120, width: 150, height: 150)
 
+// draw rounded rectangle
+canvas.fillColor = Color.init(hue: 47, saturation: 100, brightness: 100, alpha: 100)
+canvas.drawRoundedRectangle(centreX: 320, centreY: 160, width: 120, height: 140)
+//draw rectangle
+canvas.fillColor = Color.init(hue: 225, saturation: 100, brightness: 100, alpha: 90)
+canvas.drawRectangle(bottomLeftX: 170, bottomLeftY: 20, width: 120, height: 300)
+//draw a triangle
+canvas.fillColor = Color.init(hue: 127, saturation: 100, brightness: 100, alpha: 80)
+var points : [NSPoint] = []
+points.append( NSPoint(x:410,y:50))
+points.append( NSPoint(x:340,y:250))
+points.append( NSPoint(x:490,y:250))
 
+canvas.drawCustomShape(with: points)
 /*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
- */
+// ## Template code
+// The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+// */
 PlaygroundPage.current.liveView = canvas.imageView
